881a5520bfacdad82e11c14e6b05c7b41015c510
Stephendass <stephen.stainluss@gmail.com>
2024-05-27 23:20:58 +0530

    executions part in js

---
+++ JS_Class2/execution.js
@@ -0,0 +1,25 @@
+var a = 2
+var b = 3
+
+
+function addition(n1, n2){
+    var sum = n1 + n2
+    return sum
+}
+
+let ans = addition(a, b)
+console.log(ans)
+
+//square a number
+var n=10;
+function square(n){
+var ans=n*n;
+return ans;
+} 
+
+var square1 = square(n);
+var square2 = square(16); 
+
+
+console.log(square1)
+console.log(square2)
