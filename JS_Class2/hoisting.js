d9a6a7938cff30c8b993e8827497e814b43564ac
Stephendass <stephen.stainluss@gmail.com>
2024-05-27 23:41:15 +0530

    Hoisting in JS

---
+++ JS_Class2/hoisting.js
@@ -0,0 +1,11 @@
+console.log(a)// Intially its undefined => Hoisted with value undefined
+
+
+printName('JS') // This function will executed
+var a = 'Stephen'
+
+function printName(name){
+    console.log(name)
+}
+
+
