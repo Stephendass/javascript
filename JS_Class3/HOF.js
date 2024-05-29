ebd4a5032cc7ec5b01704d9a853d8b2e3b84ff89
Stephendass <stephen.stainluss@gmail.com>
2024-05-29 23:33:28 +0530

    Using HOF

--- JS_Class3/HOF.js
+++ JS_Class3/HOF.js
@@ -9,3 +9,24 @@ function calculate(arr, cb) {
     return result;
 }
 
+function circleArea(radius) {
+    return 3.14 * radius * radius;
+}
+
+function circleCircumference(radius) {
+    return 2 * 3.14 * radius;
+}
+
+function circleDiameter(radius) {
+    return 2 * radius;
+}
+
+// calculate the area for each radius
+
+let Areas = calculate(radiusArr, circleArea);
+let circumference = calculate(radiusArr, circleCircumference);
+let diameter = calculate(radiusArr, circleDiameter);
+
+console.log(Areas);
+console.log(circumference);
+console.log(diameter);
