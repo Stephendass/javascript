35e989fbf56335a7b7ff91201ff2b35a6f1402fc
Stephendass <stephen.stainluss@gmail.com>
2024-05-30 17:11:17 +0530

    A function that reduces an array to a single value based on a given function.

--- JS_Class3/reduce.js
+++ JS_Class3/reduce.js
@@ -1,1 +1,27 @@
-let numbersArr = [1 , 2, 3, 4 ,5]
+let numbersArr = [1, 2, 3, 4 ,5]
+
+//calculate the sum of all the array element
+//basic
+function sumAll(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}       
+
+console.log(sumAll(numbersArr))
+
+let addResult = numbersArr.reduce(function(acc, num){
+    acc = acc + num;
+    return acc
+}, 0)
+
+
+let prodResult = numbersArr.reduce(function(acc, num){
+    acc = acc * num;
+    return acc
+}, 1)
+
+console.log(addResult)
+console.log(prodResult)
