055c014b26d6dcf41d7ad355981cc5f88c9b5503
Stephendass <stephen.stainluss@gmail.com>
2025-02-20 00:37:45 +0530

    Format Documents

--- JS_Class3/filter.js
+++ JS_Class3/filter.js
@@ -1,9 +1,9 @@
-let NumbersArr = [2 , 4 , 7 , 8 , 10 , 11 , 13 , 14]
+let NumbersArr = [2, 4, 7, 8, 10, 11, 13, 14]
 
-function getEven(arr){
+function getEven(arr) {
     let evenNum = []
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] % 2 == 0){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
             evenNum.push(arr[i])
         }
     }
@@ -17,7 +17,7 @@ console.log(result);
 
 //filter
 
-let eveArr = NumbersArr.filter(function(num){
+let eveArr = NumbersArr.filter(function (num) {
     return num % 2 == 0
 })
 console.log(eveArr);
@@ -26,7 +26,7 @@ console.log(eveArr);
 
 const transactions = [1000, 3000, 4000, 2000, -989, 3800, -4500]
 
-let ans = transactions.filter(function(num1){
+let ans = transactions.filter(function (num1) {
     return num1 > 0
 })
 
--- JS_Class3/reduce.js
+++ JS_Class3/reduce.js
@@ -1,24 +1,24 @@
-let numbersArr = [1, 2, 3, 4 ,5]
+let numbersArr = [1, 2, 3, 4, 5]
 
 //calculate the sum of all the array element
 //basic
-function sumAll(arr){
+function sumAll(arr) {
     let sum = 0;
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         sum = sum + arr[i];
     }
     return sum;
-}       
+}
 
 console.log(sumAll(numbersArr))
 
-let addResult = numbersArr.reduce(function(acc, num){
+let addResult = numbersArr.reduce(function (acc, num) {
     acc = acc + num;
     return acc
 }, 0)
 
 
-let prodResult = numbersArr.reduce(function(acc, num){
+let prodResult = numbersArr.reduce(function (acc, num) {
     acc = acc * num;
     return acc
 }, 1)
